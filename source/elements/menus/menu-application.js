2cb42306ab08a1d5b8c7c40afe7fdebf9ac166e1
Chris v. <cavandenhurk@gmail.com>
2025-07-15 16:52:23 -0400

    Bug fix for transfer applications pulling incorrect application id.

--- source/elements/menus/menu-application.js
+++ source/elements/menus/menu-application.js
@@ -56,7 +56,7 @@ const data =
         {
             const { client: { clanManagement: { applications, cache: { active }}}, member} = interaction
             
-            const transfers = applications.getTransfersByClan(active.get(member.id)).sort((a, b) => a.timestamp - b.timestamp).reduce((obj, application) => { obj[template.menu(application)] = application.id; return obj }, {})
+            const transfers = applications.getTransfersByClan(active.get(member.id)).sort((a, b) => a.timestamp - b.timestamp).reduce((obj, application) => { obj[template.menu(application)] = application.member.id; return obj }, {})
             
             return Component
                 .menu(this.meta.id)
