210b0d871af75b15d61e0fc65318ee0d580b9666
Chris v. <cavandenhurk@gmail.com>
2025-11-15 00:44:43 -0500

    Removing logging from prod

--- start.bat
+++ start.bat
@@ -1,4 +1,9 @@
 @echo off
 cd C:\coding\project\geo\prod
-pm2 start Geo
-pm2 log Geo
+
+call "C:\Users\sun\AppData\Roaming\npm\pm2.cmd" start Geo
+
+if errorlevel 1 (
+    call "C:\Users\sun\AppData\Roaming\npm\pm2.cmd" start main.js --name Geo
+    call "C:\Users\sun\AppData\Roaming\npm\pm2.cmd" save
+)
